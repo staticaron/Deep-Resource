3e1db6a029c25b449618791f05a9fcba057f7d88
Devanshu Mishra <mishradev1910@gmail.com>
2021-04-26 01:20:37 +0530

    Main Menu Works

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    private string levelToLoad;
+
+    private void Start()
+    {
+        levelToLoad = "Level" + PlayerPrefs.GetInt("BestLevel").ToString();
+        print(levelToLoad);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            TransitionAndLevelLoader.instance.PlayEndTransitionAndLoadLevel(levelToLoad);
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Application.Quit();
+        }
+    }
+}
